f7085fadbd2c51531e7d4dafed6b6cf1dc868fbe
mar1n <30738216+mar1n@users.noreply.github.com>
2022-04-15 18:08:10 +0100

    install test library, first test is this friday

---
+++ Friday.test.js
@@ -0,0 +1,14 @@
+
+
+
+describe('Friday the thirtTheents', () => {
+    test('Is this a friday?', () => { 
+        let someYear = '1999';
+        let year = new Date();
+        year.setFullYear(someYear);
+        year.setMonth(7);
+        console.log('year', year);
+        year.setDate(13);
+        expect(year.getDay()).toEqual(5);
+    })
+});
